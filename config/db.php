4bda038f44f884a5e2ef2a7bbdcacf57416f4d3c
pilotftzi <pilotftzi@gmail.com>
2013-04-24 08:27:59 +0300

    all

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+if (!defined('VIEW'))
+    die("Hacking attempt");
+
+/**
+ * Данные для соединения с базой данных
+ * @author dandelion <web.dandelion@gmail.com>
+ */
+$dbms       = 'mysql';
+$dblocation = 'localhost';
+$dbuser     = 'root';
+$dbpasswd   = '';
+$dbname     = 'foxweb';
