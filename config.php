2c6c1ac787ea6c214cb36bf6e3ec5618a61a03df
andreybolonin <andreybolonin1989@gmail.com>
2016-04-28 10:59:36 +0300

    added pagekit

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php return array (
+  'application' => 
+  array (
+    'debug' => false,
+  ),
+  'database' => 
+  array (
+    'default' => 'mysql',
+    'connections' => 
+    array (
+      'mysql' => 
+      array (
+        'dbname' => 'pagekit',
+        'host' => 'localhost',
+        'user' => 'root',
+        'prefix' => 'pk_',
+        'password' => 'root',
+      ),
+    ),
+  ),
+  'system' => 
+  array (
+    'secret' => 'HKhU/D5dWKAeHFIeTcW4FC1IfISDJcUE8yu5uX/9tvDba9tsIvxUzqkkq67v1ahx',
+  ),
+);
